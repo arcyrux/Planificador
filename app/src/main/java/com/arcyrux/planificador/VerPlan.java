0bb2fa04489cea9f6aae0bfee4736a1f92b13de6
Martin <arcyrux@gmail.com>
2019-03-11 13:16:49 -0300

    inicial

---
+++ app/src/main/java/com/arcyrux/planificador/VerPlan.java
@@ -0,0 +1,13 @@
+package com.arcyrux.planificador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_plan);
+    }
+}
