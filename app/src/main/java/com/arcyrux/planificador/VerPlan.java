784c166465db057fd52feb5597f12307892f7531
Martin <arcyrux@gmail.com>
2019-03-11 22:09:31 -0300

    Insertada linea pierna sin desnivel en waypoint

--- app/src/main/java/com/arcyrux/planificador/VerPlan.java
+++ app/src/main/java/com/arcyrux/planificador/VerPlan.java
@@ -1,7 +1,11 @@
 package com.arcyrux.planificador;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class VerPlan extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class VerPlan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_plan);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setContentView(R.layout.activity_ver_plan);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout parent = findViewById(R.id.parent);
+
+        View linea = inflater.inflate(R.layout.linea_pierna, null);
+        parent.addView(linea);
+
+        View linea1 = inflater.inflate(R.layout.linea_pierna, null);
+        parent.addView(linea1);
     }
 }
